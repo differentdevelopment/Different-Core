3125dd0e984a415f2b9a1bf23fe76922e7c2c5f6
Kálóczi Kevin <38252773+kaloczikvn@users.noreply.github.com>
2022-07-29 11:27:31 +0200

    Update 2021_10_17_110000_create_login_tokens_table.php

--- src/database/migrations/2021_10_17_110000_create_login_tokens_table.php
+++ src/database/migrations/2021_10_17_110000_create_login_tokens_table.php
@@ -19,7 +19,7 @@ class CreateLoginTokensTable extends Migration
             $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
             $table->string('token')->unique();
             $table->timestamp('consumed_at')->nullable();
-            $table->timestamp('expires_at');
+            $table->timestamp('expires_at')->nullable();
             $table->timestamps();
         });
     }
