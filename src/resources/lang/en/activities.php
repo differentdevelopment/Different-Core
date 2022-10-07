d7e8c79e15a082d7db742ab2013ffe19fc92f345
Kálóczi Kevin <kaloczi.kvn@gmail.com>
2022-10-07 09:36:45 +0200

    added english translations

--- src/resources/lang/en/activities.php
+++ src/resources/lang/en/activities.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'activity' => 'Aktivitás',
-    'activities' => 'Aktivitások',
-    'created_at' => 'Készült ekkor',
-    'description' => 'Leírás',
-    'causer' => 'Készítette',
-    'subject_type' => 'Modell',
-    'subject_id' => 'Azonosító',
+    'activity' => 'Activity',
+    'activities' => 'Activities',
+    'created_at' => 'Created at',
+    'description' => 'Description',
+    'causer' => 'Causer',
+    'subject_type' => 'Model',
+    'subject_id' => 'Id',
 ];
