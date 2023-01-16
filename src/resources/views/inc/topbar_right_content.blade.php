20df15a88ccf9bd3d8a395ef0efb96216d7c4dda
kulcsarbence <32749705+kulcsarbence@users.noreply.github.com>
2023-01-16 16:44:08 +0100

    Update topbar_right_content.blade.php

--- src/resources/views/inc/topbar_right_content.blade.php
+++ src/resources/views/inc/topbar_right_content.blade.php
@@ -3,7 +3,7 @@
 @if (session('account_id') && backpack_user()->selectable_accounts->count() > 1)
     <li class="nav-item d-md-down-none mr-2">
         <button class="btn btn-light dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-            {{ backpack_user()->selectable_accounts->firstWhere('id', session('account_id'))->name }}
+            {{ backpack_user()->selectable_accounts->firstWhere('id', session('account_id'))?->name }}
         </button>
         <div class="dropdown-menu dropdown-menu-right py-0">
             @foreach (backpack_user()->selectable_accounts as $account)
